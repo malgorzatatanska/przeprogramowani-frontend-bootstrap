70f3cc1b1745b4885031500f8b277e1b9d3f401e
malgorzatatanska <malgosiatanska1@gmail.com>
2024-03-31 23:57:16 +0200

    added comment into github action

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ const App = () => {
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
-        <h1 className="text-xl font-bold text-center">Rick and Morty - Fan Service !!! test</h1>
+        <h1 className="text-xl font-bold text-center">Rick and Morty - Fan Service !!!</h1>
+        <h2>Test</h2>
       </div>
       <Outlet />
     </div>
